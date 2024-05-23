ec40e235cbcdf0d50a9ca287929ab444cd4101f9
Arda Yildirim <ardayildirim05@icloud.com>
2024-05-23 06:32:55 +0200

    Create status enum

--- src/main/java/com/example/LiveChat/chat/ChatMessage.java
+++ src/main/java/com/example/LiveChat/chat/ChatMessage.java
@@ -1,4 +1,11 @@
 package com.example.LiveChat.chat;
 
+// This is a chat message model class, which follows the MVC design pattern
 public class ChatMessage {
+    private String senderName;
+    private String receiverName;
+    private String message;
+    private String date;
+
+    private Status en
 }
---
+++ src/main/java/com/example/LiveChat/chat/Status.java
@@ -0,0 +1,8 @@
+package com.example.LiveChat.chat;
+
+import lombok.*;
+public enum Status {
+    JOIN,
+    MESSAGE,
+    LEAVE
+}
