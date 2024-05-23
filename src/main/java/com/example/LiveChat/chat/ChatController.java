72c24908980a3f57b1cbe760bbce83d0f0059081
Arda Yildirim <ardayildirim05@icloud.com>
2024-05-23 07:18:57 +0200

    Implement ChatController

--- src/main/java/com/example/LiveChat/chat/ChatController.java
+++ src/main/java/com/example/LiveChat/chat/ChatController.java
@@ -1,4 +1,27 @@
 package com.example.LiveChat.chat;
 
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+
+
+// This is a rest controller for chat messages, which handles the network calls related to chats
 public class ChatController {
+    // Springboot will handle the dependency injection for us.
+    private SimpMessagingTemplate simpMessagingTemplate;
+
+    // Endpoint for public chats
+    @MessageMapping("/message") // Receive endpoint
+    @SendTo("chatroom/public") // Send endpoint
+    public ChatMessage receivePublicMessage(@Payload ChatMessage message) {
+        return message;
+    }
+
+    @MessageMapping("/private-message")
+    public ChatMessage receivePrivateMessage(@Payload ChatMessage message) {
+        simpMessagingTemplate.convertAndSendToUser(message.getReceiverName(), "/private", message); // user/UserName/private
+
+        return message;
+    }
 }
