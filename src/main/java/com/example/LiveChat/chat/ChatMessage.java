38110b1c385f64ec835d04fbed08280dc818b76a
Arda Yildirim <ardayildirim05@icloud.com>
2024-05-23 06:33:26 +0200

    Implement ChatMessage

--- src/main/java/com/example/LiveChat/chat/ChatMessage.java
+++ src/main/java/com/example/LiveChat/chat/ChatMessage.java
@@ -1,11 +1,18 @@
 package com.example.LiveChat.chat;
 
 // This is a chat message model class, which follows the MVC design pattern
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class ChatMessage {
     private String senderName;
     private String receiverName;
     private String message;
     private String date;
-
-    private Status en
+    private Status status;
 }
