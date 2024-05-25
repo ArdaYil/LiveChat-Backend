bcf59252e7e6d145438fe997235aa074d45c1e70
Arda Yildirim <ardayildirim05@icloud.com>
2024-05-25 11:56:42 +0200

    Fix bug where ChatController was not set up as a bean for springboot dependency injection

--- src/main/java/com/example/LiveChat/chat/ChatController.java
+++ src/main/java/com/example/LiveChat/chat/ChatController.java
@@ -1,19 +1,29 @@
 package com.example.LiveChat.chat;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.socket.messaging.SessionSubscribeEvent;
+
+import java.util.Date;
 
 
 // This is a rest controller for chat messages, which handles the network calls related to chats
+@Controller
 public class ChatController {
     // Springboot will handle the dependency injection for us.
+
+    @Autowired
     private SimpMessagingTemplate simpMessagingTemplate;
 
     // Endpoint for public chats
     @MessageMapping("/message") // Receive endpoint
-    @SendTo("chatroom/public") // Send endpoint
+    @SendTo("/chatroom/public") // Send endpoint
     public ChatMessage receivePublicMessage(@Payload ChatMessage message) {
         return message;
     }
@@ -22,6 +32,7 @@ public class ChatController {
     public ChatMessage receivePrivateMessage(@Payload ChatMessage message) {
         simpMessagingTemplate.convertAndSendToUser(message.getReceiverName(), "/private", message); // user/UserName/private
 
+        System.out.println(message.getMessage());
         return message;
     }
 }
